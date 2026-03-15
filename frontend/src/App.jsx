07b3f8fcd4092a650b6fef173ac52019d5f332e9
Tejas <tejasburkul967@gmail.com>
2026-03-16 01:08:28 +0530

    added duck dns

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
 
     try {
 
-      const res = await fetch("http://13.204.87.199:5000/api/chat", {
+      const res = await fetch("http://tejas-chatbot.duckdns.org:5000/api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
